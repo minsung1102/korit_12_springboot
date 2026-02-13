727c66a26dcf76fa29479ad0fea34e59bc0f195b
minsung1102 <kms38400097@gmail.com>
2026-02-13 10:36:07 +0900

    first commit

---
+++ springbootstudy/src/main/java/com/temp/springbootstudy/domain/AppUser.java
@@ -0,0 +1,25 @@
+package com.temp.springbootstudy.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name, username, password;
+
+    public AppUser() {}
+
+    public AppUser(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+}
