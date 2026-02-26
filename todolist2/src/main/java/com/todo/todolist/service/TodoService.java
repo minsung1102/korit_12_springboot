aa2449911ec8d537a212fb1be7e641485f30a708
minsung1102 <kms38400097@gmail.com>
2026-02-26 17:44:51 +0900

    feat : 20260226

---
+++ todolist2/src/main/java/com/todo/todolist/service/TodoService.java
@@ -0,0 +1,40 @@
+package com.todo.todolist.service;
+
+import com.todo.todolist.dto.TodoRequest;
+import com.todo.todolist.dto.TodoResponse;
+import com.todo.todolist.entity.Todo;
+import com.todo.todolist.entity.User;
+import com.todo.todolist.repository.TodoRepository;
+import com.todo.todolist.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TodoService {
+    private final TodoRepository todoRepository;
+    private final UserRepository userRepository;
+
+    // Post 요청을 하는 비지니스 로직 작성
+    public TodoResponse createTodo(Long userId, TodoRequest request) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+        // DTO -> Entity로의 변환
+        Todo todo = new Todo(request.content(), user);  // Todo 객체 만들어지면 isCompleted = false가 적용됩니다.
+        Todo savedTodo = todoRepository.save(todo);
+
+        // Entity → DTO로 변환해서 Controller로 넘겨줄겁니다.
+        return new TodoResponse(savedTodo.getId(), savedTodo.getContent(), savedTodo.isCompleted());
+    }
+
+    // GET 요청을 하는 비지니스 로직 작성
+    @Transactional(readOnly = true)
+    public List<TodoResponse> getTodoList() {
+        return todoRepository.findAll().stream()
+                .map(todo -> new TodoResponse(todo.getId(), todo.getContent(), todo.isCompleted()))
+                .collect(Collectors.toList());
+    }
+}
